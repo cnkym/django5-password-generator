415467eac2b94dc473f9454e267d571d34133c5a
T. Nakayama <chin.nakayama@outlook.jp>
2024-05-12 17:51:36 +0900

    v01: using template html file for views

--- generator/views.py
+++ generator/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse # v01.for return a http response message
+
 
 # Create your views here.
 
 def home(requests): # v01.add a function for Home page
-    return HttpResponse("Hello World!")
+    return render(requests, # v01.use tamplate
+                  'generator/home.html',    # template html file
+                  {'msg' : "Hello World!"}, # dict format message
+                  )
