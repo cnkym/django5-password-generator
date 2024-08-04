42e3a07b53ed4310d3b15ede79a7dd6db62479e1
T. Nakayama <chin.nakayama@outlook.jp>
2024-08-04 11:25:23 +0900

    v04.add generator

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import random
 
 # Create your views here.
 
@@ -10,6 +10,22 @@ def home(requests): # v01.add a function for Home page
                   )
 
 def password(requests): # v03.add a form forpassword page
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    if requests.GET.get('numbers'):
+        characters.extend(list("123456789"))
+    if requests.GET.get('special'):
+        characters.extend(list("!#$%&"))
+    if requests.GET.get('uppercase'):
+        characters.extend(list("ABCDEFGHIJKLMNPQRSTUVWXYZ"))
+
+    length = int(requests.GET.get('length',8))
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
     return render(requests,
                   'generator/password.html',    # passwoed html page
+                  {'password' : thepassword},   # dict format message
                   )
